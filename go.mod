cb29a464836261237a94987aab3b9a2808d37274
Simon Murray <spjmurray@yahoo.co.uk>
2025-11-11 09:18:57 +0000

    Add Lister Helpers (#328)
    When we list resources, especially with the new APIs that don't require
    an organization and project in the path, we would have to list literally
    every resource in the system, then filter out using ReBAC, which is
    horribly inefficient and, more pertinently, pretty insecure.  Instead we
    should use label selection and the joys of database indexes to do the
    heavy lifting.
    
    We do provide query selectors for organization and project to limit the
    scope, but there is no requirement to actually use these.  Now we have
    unscoped ACLs however, the ACL itself contains all the organizations and
    projects the user has access to, so we can do a number of things.  For a
    start we can mandate a default organization selection based on the ACL,
    so you're never getting anything that belongs to someone else.
    Secondly, when you do specify a selector, we can constrain this to only
    organizations in the ACL, thus preventing attacks where you input the
    organization ID of another user.
    
    The same applies to projects.

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/oapi-codegen/runtime v1.1.1
 	github.com/spf13/pflag v1.0.6
+	github.com/spjmurray/go-util v0.1.3
 	github.com/stretchr/testify v1.10.0
 	github.com/unikorn-cloud/core v1.10.0
 	go.opentelemetry.io/otel v1.35.0
--- pkg/rbac/handler.go
+++ pkg/rbac/handler.go
@@ -20,9 +20,15 @@ import (
 	"context"
 	"slices"
 
+	"github.com/spjmurray/go-util/pkg/set"
+
+	"github.com/unikorn-cloud/core/pkg/constants"
 	"github.com/unikorn-cloud/core/pkg/server/errors"
 	unikornv1 "github.com/unikorn-cloud/identity/pkg/apis/unikorn/v1alpha1"
 	"github.com/unikorn-cloud/identity/pkg/openapi"
+
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
 )
 
 // operationAllowedByEndpoints iterates through all endpoints and tries to match the required name and
@@ -149,4 +155,166 @@ func AllowRole(ctx context.Context, role *unikornv1.Role, organizationID string)
 	}
 
 	return nil
+}
+
+// OrganizationIDs returns a list of all organization IDs from the ACL for the purposes
+// of limiting list type API operations.
+func OrganizationIDs(ctx context.Context) []string {
+	acl := FromContext(ctx)
+
+	if acl.Organizations == nil {
+		return nil
+	}
+
+	organizations := *acl.Organizations
+
+	if len(organizations) == 0 {
+		return nil
+	}
+
+	organizationIDs := make([]string, len(organizations))
+
+	for i := range organizations {
+		organizationIDs[i] = organizations[i].Id
+	}
+
+	return organizationIDs
+}
+
+// ProjectIDs returns a list of all projects from a single organization in the ACL for
+// the purposes of limiting list type API operations.
+func ProjectIDs(ctx context.Context, organizationID string) []string {
+	acl := FromContext(ctx)
+
+	if acl.Organizations == nil {
+		return nil
+	}
+
+	organizations := *acl.Organizations
+
+	if len(organizations) == 0 {
+		return nil
+	}
+
+	index := slices.IndexFunc(organizations, func(o openapi.AclOrganization) bool {
+		return o.Id == organizationID
+	})
+
+	if index < 0 {
+		return nil
+	}
+
+	organization := organizations[index]
+
+	if organization.Projects == nil {
+		return nil
+	}
+
+	projects := *organization.Projects
+
+	if len(projects) == 0 {
+		return nil
+	}
+
+	projectIDs := make([]string, len(projects))
+
+	for i := range projects {
+		projectIDs[i] = projects[i].Id
+	}
+
+	return projectIDs
+}
+
+// AddQuery adds a set of query values to a label selector.
+func AddQuery(selector labels.Selector, label string, vals []string) (labels.Selector, error) {
+	if len(vals) == 0 {
+		return selector, nil
+	}
+
+	if len(vals) == 1 {
+		req, err := labels.NewRequirement(label, selection.Equals, vals)
+		if err != nil {
+			return nil, err
+		}
+
+		return selector.Add(*req), nil
+	}
+
+	req, err := labels.NewRequirement(label, selection.In, vals)
+	if err != nil {
+		return nil, err
+	}
+
+	return selector.Add(*req), nil
+}
+
+// AddOrganizationIDQuery adds an organizational query selector that limits resources to
+// be listed to those available in the ACL and optionally constrained to those in the
+// request query using a boolean intersection.
+func AddOrganizationIDQuery(ctx context.Context, selector labels.Selector, query []string) (labels.Selector, error) {
+	// NOTE: super-admin accounts and system accounts will not have any organizations
+	// defined in the ACL, so we let this slide, and trust they will add a query to limit
+	// the scope.  It should not be possible for a user to get here without being a
+	// member of an organization, but ReBAC will prevent any unintended reads.
+	organizationIDs := OrganizationIDs(ctx)
+	if len(organizationIDs) == 0 {
+		return AddQuery(selector, constants.OrganizationLabel, query)
+	}
+
+	if len(query) > 0 {
+		organizationIDs = slices.Collect(set.New(organizationIDs...).Intersection(set.New(query...)).All())
+	}
+
+	return AddQuery(selector, constants.OrganizationLabel, organizationIDs)
+}
+
+// AddOrganizationAndProjectIDQuery gets all organizationIDs the user can access (or has requested
+// explicit and has access to), then selects all projects that can be accessed.  If en explicit
+// project query has been provided, then constrain the accessible project set.
+func AddOrganizationAndProjectIDQuery(ctx context.Context, selector labels.Selector, organizationQuery []string, projectQuery []string) (labels.Selector, error) {
+	// NOTE: super-admin accounts and system accounts will not have any organizations
+	// defined in the ACL, so we let this slide, and trust they will add a query to limit
+	// the scope.  It should not be possible for a user to get here without being a
+	// member of an organization, but ReBAC will prevent any unintended reads.
+	organizationIDs := OrganizationIDs(ctx)
+	if len(organizationIDs) == 0 {
+		selector, err := AddQuery(selector, constants.OrganizationLabel, organizationQuery)
+		if err != nil {
+			return nil, err
+		}
+
+		return AddQuery(selector, constants.ProjectLabel, projectQuery)
+	}
+
+	if len(organizationQuery) > 0 {
+		organizationIDs = slices.Collect(set.New(organizationIDs...).Intersection(set.New(organizationQuery...)).All())
+	}
+
+	// Create a set of all projects that the user can access across the selected
+	// organizations.
+	projectIDSet := set.New[string]()
+
+	for _, organizationID := range organizationIDs {
+		projectIDSet = projectIDSet.Union(set.New(ProjectIDs(ctx, organizationID)...))
+	}
+
+	if len(projectQuery) > 0 {
+		projectIDSet = projectIDSet.Intersection(set.New(projectQuery...))
+	}
+
+	projectIDs := slices.Collect(projectIDSet.All())
+
+	var err error
+
+	selector, err = AddQuery(selector, constants.OrganizationLabel, organizationIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	selector, err = AddQuery(selector, constants.ProjectLabel, projectIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	return selector, nil
 }
